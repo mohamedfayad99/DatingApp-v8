e7e5d0ca71977c7f5e285a5ac79bf8ef7d2cf668
mohamedfayad99 <mofayad992001@gmail.com>
2025-01-02 23:48:10 +0200

    Building awalking skeleton - Angular

--- API/Program.cs
+++ API/Program.cs
@@ -4,10 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddData(builder.Configuration);
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("http://localhost:4200","https://localhost:4200"));
+            
 app.Run();
+
